6cdb41dcd52ecf7b74c8d2ab2de9c01f5d69a788
svgr110 <70746911+svgr110@users.noreply.github.com>
2021-04-14 16:05:41 +0800

    Update diy-1.sh

--- build/Lede_x86_64/diy-1.sh
+++ build/Lede_x86_64/diy-1.sh
@@ -17,7 +17,12 @@ git clone https://github.com/jerrykuku/node-request.git package/lean/node-reques
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus  #京东签到
 svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/lean/luci-app-passwall  #passwall出国软件
 svn co https://github.com/xiaorouji/openwrt-package/trunk/package package/lean/package  #passwall出国软件配套
-
+git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
+git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
+find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
+find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
+svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb
+git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
 rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
 #全新的[argon-主题]此主题玩法很多,这里看说明【https://github.com/jerrykuku/luci-theme-argon/blob/18.06/README_ZH.md】
 #用WinSCP连接openwrt，在/www/luci-static/argon里面创建background文件夹（如果本身就有background就不需要创建）来存放jpg png gif格式图片可以自定义登陆界面，gif图片为动态登陆界面
